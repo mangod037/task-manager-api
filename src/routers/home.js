58c641b066f7188164b066776a6e91bfdd984656
mangod037 <mango037@naver.com>
2019-10-14 11:26:17 +0900

    get users

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = new express.Router();
 
+const User = require("../models/user");
+
 router.get("/home", async (req, res) => {
-  res.send("home page");
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+  // User.find({})
+  //   .then(users => {
+  //     res.send(users);
+  //   })
+  //   .catch(error => {
+  //     res.status(500).send();
+  //   });
 });
 
 module.exports = router;
