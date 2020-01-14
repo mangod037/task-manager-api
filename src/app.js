ea17f86e33ccc30546fcab9b508ff3a3b60a72c6
mangod037 <mango037@naver.com>
2020-01-14 13:09:37 +0900

    app.js

--- src/app.js
+++ src/app.js
@@ -3,15 +3,11 @@ require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
-const homeRouter = require("./routers/home");
-
 const app = express();
 
 app.use(express.json());
 
 app.use(userRouter);
 app.use(taskRouter);
-
-app.use(homeRouter);
 
 module.exports = app;
